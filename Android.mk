32c931ba67794ff39f7c269e0af6513e842e76a4
Teemu Tuominen <teemu.tuominen@intel.com>
2014-06-04 14:18:27 +0300

    media-ctl: add libmediatext build rule into Android.mk
    BZ:182374
    
    Change-Id: I13d5f0d13a094e620463b7e3582061cbfaa34b01
    Signed-off-by: Teemu Tuominen <teemu.tuominen@intel.com>

--- Android.mk
+++ Android.mk
@@ -20,6 +20,16 @@ LOCAL_COPY_HEADERS := src/v4l2subdev.h
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := src/mediatext.c
+LOCAL_MODULE := libmediatext
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/android
+LOCAL_SHARED_LIBRARIES := libmediactl libv4l2subdev
+LOCAL_COPY_HEADERS_TO := mediactl
+LOCAL_COPY_HEADERS := src/mediatext.h
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := src/main.c src/options.c src/options.h src/tools.h
 LOCAL_MODULE := media-ctl
 LOCAL_MODULE_TAGS := optional
